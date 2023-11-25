029e0a1a8d0e33434ffe2163a577571e5c6f716a
Faisal Masood <masood.faisal@gmail.com>
2023-11-25 23:14:02 +1100

    fe service now add the response.text from the be service in its response

--- fe-app/app.py
+++ fe-app/app.py
@@ -3,7 +3,7 @@ from aiohttp import web
 
 async def root(request):
     be_response = requests.get("http://be-app.istio-demo.svc.cluster.local:8080/")
-    return web.Response(text=f"Respomse from BE App {be_response}")
+    return web.Response(text=f"Respomse from BE App {be_response.text}")
 
 app = web.Application()
 app.router.add_get('/', root)
